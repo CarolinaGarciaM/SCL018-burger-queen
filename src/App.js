f20d78fc913f3a81c47e3736e9916ca0f31a3333
CarolinaGarciaM <carolina.garciameneses@gmail.com>
2022-01-24 12:34:41 -0300

    Actualización de rutas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import Home from './Components/Home';
 import ItemBreakfast from './Components/Customer/ItemBreakfast';
+import ItemCustomer from './Components/Customer/ItemCustomer';
+import ItemMenu from './Components/Customer/ItemMenu';
 import ItemKitchen from './Components/Kitchen/ItemKitchen';
+import Footer from './Components/Commons/Footer';
+import Header from './Components/Commons/Header';
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 
@@ -14,10 +18,13 @@ function App() {
           <Route path="" element={<Home />} />
           <Route path="Home" element={<Home />} />
           <Route path="ItemBreakfast" element={<ItemBreakfast />} />
+          <Route path="ItemCustomer" element={<ItemCustomer />} />
+          <Route path="ItemMenu" element={<ItemMenu />} />
           <Route path="ItemKitchen" element={<ItemKitchen />} />
+          <Route path="Footer" element={<Footer />} />
+          <Route path="Header" element={<Header />} />
         </Routes>
       </Router>
-
     </div>
 
 
